b9e00d7d98d8ef2a5542e8f9dd9a7767fe88a27e
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-04-01 14:41:31 -0700

    made footsie

--- client/App.js
+++ client/App.js
@@ -3,12 +3,18 @@ import "@tensorflow/tfjs-backend-webgl";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 import { Landing } from "./components/Landing";
+import { Footer } from './components/Footer';
 
 const App = () => {
   return (
+    <div>
     <div id='app'>
       <Navbar />
       <Routes />
+    </div>
+    <div>
+      <Footer />
+    </div>
     </div>
   );
 };
---
+++ client/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'boxicons'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p className='footer_info'>
+        Email: rotoflo.customer.support@gmail.com <box-icon name='instagram-alt' type='logo' color='#ffffff'></box-icon> © 2021 Copyright: RotoFlo
+      </p>
+    </footer>
+  );
+};
+
+//make an insta and get people to hashtag us?
+//hold filter competitions for new coders?
+// dance competition video submission?
