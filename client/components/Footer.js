8102c2391e55fad9d9466a403a16bb92b7946c53
Serena Chang <serena.w.chang@gmail.com>
2022-04-05 18:32:16 -0400

    red email

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer>
       <p className='footer_info'>
-       <Link>Email: rotoflo.ar@gmail.com</Link>    
+       <Link style={{color:'#db4855'}}>Email: rotoflo.ar@gmail.com</Link>    
        <Link><box-icon name='instagram-alt' type='logo' color='#db4855'></box-icon></Link> 
        <Link to="/about" style={{color:'#db4855'}}>About The Developers</Link>
        © 2022 Copyright: RotoFlo
