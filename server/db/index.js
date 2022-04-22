3ca6fe43368537487467316348d5a81bdfe16d0d
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-04-21 20:37:11 -0700

    made comments on db relations clearer

--- server/db/index.js
+++ server/db/index.js
@@ -7,7 +7,7 @@ const Project = require('./models/Project');
 const Roto = require('./models/Roto');
 
 User.hasMany(Project);//this makes the association go both ways and gives us magic methods.
-Project.belongsTo(User); //the thing in the keft of belongsTo has foreign key
+Project.belongsTo(User); //the table to the left of belongsTo has foreign key
 
 Roto.hasOne(Project);
 Project.belongsTo(Roto);
